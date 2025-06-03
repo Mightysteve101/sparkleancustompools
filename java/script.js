80c5081062dfa5016c7af4d27221afaa2c0028ce
Isaac Evans <mightysteve101@gmail.com>
2025-06-03 11:29:46 -0600

    Added cloudfare turnstile

--- java/script.js
+++ java/script.js
@@ -45,16 +45,6 @@ document.addEventListener("DOMContentLoaded", function(){
         }
         item.addEventListener('click', onClick)
         }
-    /********************************
-     ***Cloudfare Bot Protection*****
-     * *****************************/
-     window.onloadTurnstileCallback = function () {
-        turnstile.render("#myWidget", {
-            sitekey: "0x4AAAAAABf-Bb9A0epeHZi3", 
-            callback: function (token) {
-                console.log(`Challenge Success ${token}`);
-            },
-        });
-    };     
+ 
 });
 
